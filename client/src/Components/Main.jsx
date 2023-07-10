db6d6f7545aed28f7ab84274ac3c447327c5b017
f1relaught <116809589+f1relaught@users.noreply.github.com>
2023-07-10 17:52:50 +0300

    Update Main.jsx

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -1,101 +1,99 @@
+
 import Passuk from "./Passuk";
 import { useEffect, useState } from "react";
 import Axios from "axios";
 
 export default function Main() {
-  
-    let showPopup = false;
-    let showCantBuyPopup = false;
-    let psukim = [];
+  const [psukim, setPsukim] = useState([]);
+  const [ot, setOt] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+  const [showCantBuyPopup, setShowCantBuyPopup] = useState(false);
 
-    const [ot, setOt] = useState([]);
-    const [searchInput, setSearchInput] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
-    
-    useEffect(() => {
-      Axios.get("http://localhost:3001/api/getPsukim").then((response) => {
-        psukim = response.data;
-        console.log(psukim);
-      });
-    }, []);
-    
-    const handleChange = async () => {
-        try {
-          const response = await Axios.get(`http://localhost:3001/api/searchOtyot?ot=${searchInput}`);
-          setOt(response.data);
-        
-          if (ot) {
-            const status = ot[0].status;
-            if (status) {
-              showPopup = true;
-            } else {
-              showCantBuyPopup = true;
-            }
-          } 
-        } catch (error) {
-          console.error('Error:', error);
-          // Handle error
-        }
-      };
-      
-      const handleBuyLetter = async () => {
-        try {
-          // Update the client-side state and display success message
-          setSuccessMessage("You have successfully bought the letter.");
-          setShowPopup(false);
-          setSearchInput("");
-        } catch (error) {
-          console.error('Error:', error);
-          // Handle error
-        }
-      };
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/getPsukim").then((response) => {
+      setPsukim(response.data);
+    });
+  }, []);
 
-    return (
-      <>
+  const handleChange = async () => {
+    try {
+      const response = await Axios.get(
+        `http://localhost:3001/api/searchOtyot?ot=${searchInput}`
+      );
+      setOt(response.data);
+
+      if (ot) {
+        const status = ot[0].status;
+        if (status) {
+          setShowPopup(true);
+        } else {
+          setShowCantBuyPopup(true);
+        }
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle error
+    }
+  };
+
+  const handleBuyLetter = async () => {
+    try {
+      // Update the client-side state and display success message
+      setSuccessMessage("You have successfully bought the letter.");
+      setShowPopup(false);
+      setSearchInput("");
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle error
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <h1>חפש לפי אותיות פנויות</h1>
         <div>
-            <h1>חפש לפי אותיות פנויות</h1>
-            <div>
-                <input
-                type="text"
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                placeholder="הכנס אותיות פנויות"
-                />
-                <button onClick={handleChange}>חפש</button>
-            </div>
-            {showPopup && (
-                <div className="fixed inset-0 flex items-center justify-center z-50">
-                    <div className="bg-white p-8 rounded-lg shadow">
-                        <h2 className="text-2xl font-bold mb-4">Buy Letter</h2>
-                        <p className="mb-4">Are you sure you want to buy this letter?</p>
-                        <button
-                        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                        onClick={handleBuyLetter}
-                        >
-                        Buy
-                        </button>
-                    </div>
-                </div>
-            )}
-            {showCantBuyPopup && (
-                <div className="fixed inset-0 flex items-center justify-center z-50">
-                    <div className="bg-white p-8 rounded-lg shadow">
-                        <h2 className="text-2xl font-bold mb-4">Can't Buy Letter</h2>
-                        <p className="mb-4">!This letter is not available to be bought</p>
-                    </div>
-                </div>
-            )}
-            {successMessage && (
-                <div className="success-message text-center">{successMessage}</div>
-            )}
-            
-            {/* Render the Passuk components */}
-            {
-                psukim.map((passuk) => {
-                    return <Passuk key={passuk._id} passuk={passuk} />
-                }
-            )}
+          <input
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder="הכנס אותיות פנויות"
+          />
+          <button onClick={handleChange}>חפש</button>
         </div>
+        {showPopup && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white p-8 rounded-lg shadow">
+              <h2 className="text-2xl font-bold mb-4">Buy Letter</h2>
+              <p className="mb-4">Are you sure you want to buy this letter?</p>
+              <button
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                onClick={handleBuyLetter}
+              >
+                Buy
+              </button>
+            </div>
+          </div>
+        )}
+        {showCantBuyPopup && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="bg-white p-8 rounded-lg shadow">
+              <h2 className="text-2xl font-bold mb-4">Can't Buy Letter</h2>
+              <p className="mb-4">!This letter is not available to be bought</p>
+            </div>
+          </div>
+        )}
+        {successMessage && (
+          <div className="success-message text-center">{successMessage}</div>
+        )}
+
+        {/* Render the Passuk components */}
+        {psukim.map((passuk) => {
+          return <Passuk key={passuk._id} passuk={passuk} />;
+        })}
+      </div>
     </>
-    );
+  );
 }
