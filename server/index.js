7fc4e883df042d0a8c93ce12033cf129f7cfb028
f1relord <f1relaught@gmail.com>
2023-07-11 19:09:19 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -107,8 +107,8 @@ app.post("/api/database", async (req, res) => {
 });
 
 const httpsOptions = {
-  key: fs.readFileSync("/etc/letsencrypt/live/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/fullchain.pem"),
+  key: fs.readFileSync("certs/privkey.pem"),
+  cert: fs.readFileSync("certs/cert.pem"),
 };
 
 https.createServer(httpsOptions, app).listen("3001", () => {
