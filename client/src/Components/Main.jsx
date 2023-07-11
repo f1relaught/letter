a422ec0803c1404d57b87cc958b9b3b491cb646f
f1relord <f1relaught@gmail.com>
2023-07-11 21:34:05 +0300

    dsvvs

--- client/src/Components/Main.jsx
+++ client/src/Components/Main.jsx
@@ -11,35 +11,43 @@ export default function Main() {
   const [showCantBuyPopup, setShowCantBuyPopup] = useState(false);
 
   useEffect(() => {
-    Axios.get("https://letter-api-1:3001/api/getPsukim").then((response) => {
-      setPsukim(response.data.psukim);
-      console.log(response.data)
-    });
+    Axios.get("http://localhost:3001/api/getPsukim")
+      .then((response) => {
+        setPsukim(response.data.psukim);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle error
+      });
   }, []);
+
+  useEffect(() => {
+    if (ot.length > 0) {
+      const status = ot[0].status;
+      console.log(ot);
+      if (status) {
+        setShowPopup(true);
+      } else {
+        setShowCantBuyPopup(true);
+      }
+    }
+  }, [ot]);
 
   const handleChange = async () => {
     try {
       const response = await Axios.get(
-        `https://letter-api-1:3001/api/searchOtyot?ot=${searchInput}`
+        `http://localhost:3001/api/searchOtyot?ot=${searchInput}`
       );
       setOt(response.data);
-
-      if (ot) {
-        const status = ot[0].status;
-												console.log(ot)
-        if (status) {
-          setShowPopup(true);
-        } else {
-          setShowCantBuyPopup(true);
-        }
-      }
+      console.log(ot);
     } catch (error) {
       console.error("Error:", error);
       // Handle error
     }
   };
 
-  const handleBuyLetter = async () => {
+  const handleBuyLetter = async (ot) => {
     try {
       // Update the client-side state and display success message
       setSuccessMessage("You have successfully bought the letter.");
@@ -71,7 +79,7 @@ export default function Main() {
               <p className="mb-4">Are you sure you want to buy this letter?</p>
               <button
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                onClick={handleBuyLetter}
+                onClick={() => handleBuyLetter(ot)}
               >
                 Buy
               </button>
--- server/index.js
+++ server/index.js
@@ -1,124 +1,101 @@
-	import express from "express";
-	import mongoose from "mongoose";
-	import Psukim from "./models/Passuks.js";
-	import Otyot from "./models/Otyot.js";
-	import cors from "cors";
-	import https from "https";
-	import fs from "fs";
+import express from "express";
+import mongoose from "mongoose";
+import Psukim from "./models/Passuks.js";
+import Otyot from "./models/Otyot.js";
+import cors from "cors";
 
-	// Create the server
-	const app = express();
-	app.use(cors());
-	app.use(express.json());
+// Create the server
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-	// Connect to the database
-	mongoose.connect("mongodb://database:27017/Otyot");
+// Connect to the database
+mongoose.connect("mongodb://localhost:27017/Otyot");
 
-	// Route for the passuk's search
-	app.get("/api/getPsukim", async (req, res) => {
-	try {
-		const psukim = await Psukim.find({});
+// Route for the passuk's search
+app.get("/api/getPsukim", async (req, res) => {
+  try {
+    const psukim = await Psukim.find({});
 
-		if (psukim) {
-		res.json({ psukim });
-		console.log(psukim);
-		} else {
-		res.json({ exists: false });
-		}
-	} catch (error) {
-		console.error("Error:", error);
-		res
-		.status(500)
-		.json({ error: "An error occurred while fetching the Passuk." });
-	}
-	});
+    if (psukim) {
+      res.json({ psukim });
+      console.log(psukim);
+    } else {
+      res.json({ exists: false });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred while fetching the Passuk." });
+  }
+});
 
-	app.get("/api/getOtyot", async (req, res) => {
-	try {
-		const { passukId } = req.query;
-		const otyot = await Otyot.find({ passuk: passukId });
-		console.log(otyot);
-	} catch (error) {
-		console.error("Error:", error);
-		res
-		.status(500)
-		.json({ error: "An error occurred while fetching the Otyot." });
-	}
-	});
+app.get("/api/getOtyot", async (req, res) => {
+  try {
+    const { passukId } = req.query;
+    const otyot = await Otyot.find({ passuk: passukId });
+    console.log(otyot);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred while fetching the Otyot." });
+  }
+});
 
-	// Route for the otyot's search
-	app.get("/api/searchOtyot", async (req, res) => {
-	try {
-		const { ot } = req.query;
-		console.log(ot);
-		const otyot = await Otyot.find({ ot: ot });
+app.get("/api/searchOtyot", async (req, res) => {
+  try {
+    const { ot } = req.query;
+    const otyot = await Otyot.find({ ot: ot });
 
-		if (otyot) {
-		console.log(otyot);
-		res.json(otyot);
-		} else {
-		res.json({ exists: false });
-		}
-	} catch (error) {
-		console.error("Error:", error);
-		res
-		.status(500)
-		.json({ error: "An error occurred while fetching the Otyot." });
-	}
-	});
+    if (otyot.length > 0) {
+      console.log(otyot.ot);
+      res.json(otyot);
+    } else {
+      res.json({ exists: false });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "An error occurred while fetching the Otyot." });
+  }
+});
 
-	// Route for the generation of the database
-	app.post("/api/database", async (req, res) => {
-	try {
-		const passuk = req.body.passuk;
 
-		if (typeof passuk !== "string") {
-		return res
-			.status(400)
-			.json({ error: "Invalid sentence. Please provide a string." });
-		}
+// Route for the generation of the database
+app.post("/api/database", async (req, res) => {
+  try {
+    const passuk = req.body.passuk;
 
-		// Store the sentence in the passuks collection
-		const newPassuk = new Psukim({ passuk, otyot: [] });
-		const passukId = newPassuk._id;
+    if (typeof passuk !== "string") {
+      return res
+        .status(400)
+        .json({ error: "Invalid sentence. Please provide a string." });
+    }
 
-		// Store the otyot in the otyot collection
-		const otyot = passuk.split("");
+    // Store the sentence in the passuks collection
+    const newPassuk = new Psukim({ passuk, otyot: [] });
+    const passukId = newPassuk._id;
 
-		for (const ot of otyot) {
-		const newOtyot = new Otyot({
-			ot,
-			status: true,
-			passuk: passukId,
-		});
-		await newOtyot.save();
-		newPassuk.otyot.push(newOtyot._id);
-		}
-		await newPassuk.save();
-		res
-		.status(200)
-		.json({ message: "Passuk and otyot stored successfully." });
-	} catch (error) {
-		console.error("Error:", error);
-		res
-		.status(500)
-		.json({ error: "An error occurred while storing the passuk and otyot." });
-	}
-	});
+    // Store the otyot in the otyot collection
+    const otyot = passuk.split("");
 
-	let httpsOptions = {};
+    for (const ot of otyot) {
+      const newOtyot = new Otyot({
+        ot,
+        status: true,
+        passuk: passukId,
+      });
+      await newOtyot.save();
+      newPassuk.otyot.push(newOtyot._id);
+    }
+    await newPassuk.save();
+    res.status(200).json({ message: "Passuk and otyot stored successfully." });
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while storing the passuk and otyot." });
+  }
+});
 
-	try {
-	httpsOptions = {
-		key: fs.readFileSync("/etc/letsencrypt/live/ytzba.com/privkey.pem"),
-		cert: fs.readFileSync("/etc/letsencrypt/live/ytzba.com/cert.pem"),
-	};
-	} catch (err) {
-	console.error('Failed to load SSL certificate files:', err);
-	// Depending on how critical HTTPS is to your app, you might want to stop
-	// execution here, or fallback to HTTP.
-	}
-
-	https.createServer(httpsOptions, app).listen("3001", () => {
-	console.log("Server running on https://localhost:3001/api");
-	}).on('error', console.error);
+// Start the server using plain HTTP
+app.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001");
+});
